292d25b6400ceb1c6601ff4f60c963f74b13a875
SergeySidorchuk <serg_ss@ukr.net>
2021-07-20 00:13:35 +0300

    add Counter

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Counter from "./components/Counter/Counter";
+
+
+const App = () => (
+    <>
+        <h1>Сотояние компонента</h1>
+    <Counter initialValue={10} />
+
+        </>
+)
+
+export default App;
